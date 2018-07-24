f73e77db7718c031a876ad462ebc90f1c56f6f3d
Arun Shaji <arunshaji4230@gmail.com>
2018-07-24 22:05:11 +0530

    Updated files to deploy on heroku.

--- jukebox/settings.py
+++ jukebox/settings.py
@@ -121,4 +121,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 SLACK_TEAM = 'CBQG9DBBQ'
